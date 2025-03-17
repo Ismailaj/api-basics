f4a512c8bee36ace43f9c8d7bd5e9146fe448622
ismailaj <ismailaj664@gmail.com>
2025-03-17 15:24:57 +0100

    update js file

--- get-api/main.js
+++ get-api/main.js
@@ -1,19 +1,28 @@
-const apiUrl = 'https://fakestoreapi.com/products/1';
+// const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
-let productId = '';
 const input = document.querySelector('input');
-const products = document.querySelector('products');
-const btn = document.querySelector('btn');
+// let productId = input.value;
+const products = document.querySelector('.products');
+const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', () => {
-  productId = input.value;
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let productId = input.value;
+  const apiUrl = `https://fakestoreapi.com/products/${productId}`;
+
   fetch(apiUrl)
-    .then(response => response.json)
+    .then(response => response.json())
       .then(data => {
         products.innerHTML = `
-          `
-      })
-})
+          <h2>${data.title}</h2>
+          <img style="width:200px;" src="${data.image}">
+          <p>${data.description}</p>
+          <p>${data.price}</p>
+          <p>${data.rating.rate} (${data.rating.count} reviews)</p>
+          
+        `
+      });
+});
 
 // fetch('https://fakestoreapi.com/products/1')
 //   .then(response => response.json())
